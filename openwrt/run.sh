e6b99e619dfb3ad17ac2ec3495d57ffec88e0460
Maximilian Hils <git@maximilianhils.com>
2017-02-28 14:24:08 +0100

    add openwrt freecwmp dockerfile

---
+++ openwrt/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+freecwmp set InternetGatewayDevice.ManagementServer.PeriodicInformInterval 5
+freecwmp set InternetGatewayDevice.ManagementServer.PeriodicInformEnable 1 
+
+
+#freecwmp set InternetGatewayDevice.ManagementServer.URL http://mitm:8081/
+#freecwmp set InternetGatewayDevice.ManagementServer.Username freecwmp
+#freecwmp set InternetGatewayDevice.ManagementServer.Password freecwmp
+freecwmp set InternetGatewayDevice.ManagementServer.ConnectionRequestURL http://mitm:8081/connectionrequest
+freecwmp set InternetGatewayDevice.ManagementServer.ConnectionRequestUsername freecwmp
+freecwmp set InternetGatewayDevice.ManagementServer.ConnectionRequestPassword freecwmp
+
+ubusd &
+ubus listen &
+freecwmpd -f
